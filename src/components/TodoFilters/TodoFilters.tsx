5bc111323de2c916b5b89f9d8b654b5ea648614a
yeriel-igaw <yeriel.jeong@igaworks.com>
2021-11-02 15:21:56 +0900

    todo filter css 수정

--- src/components/TodoFilters/TodoFilters.tsx
+++ src/components/TodoFilters/TodoFilters.tsx
@@ -2,7 +2,7 @@ import React, {ChangeEvent} from 'react';
 import './TodoFilters.scss';
 import {useRecoilState} from "recoil";
 import {todoListFilterState} from "../../recoil/todo";
-import {TodoTypes} from "../../recoil/todo";
+import './TodoFilters.scss';
 
 function TodoFilters() {
     const [filter, setFilter] = useRecoilState(todoListFilterState);
@@ -11,13 +11,16 @@ function TodoFilters() {
         setFilter(event.target.value as string);
     };
     return (
-        <div>
-            Filter:
-            <select value={filter} onChange={updateFilter}>
-                <option value="Show All">All</option>
-                <option value="Show Completed">Completed</option>
-                <option value="Show Uncompleted">Uncompleted</option>
-            </select></div>
+        <div className={'filter'}>
+            <div className={'select'}>
+                <select value={filter} onChange={updateFilter}>
+                    <option value="Show All">All</option>
+                    <option value="Show Completed">Completed</option>
+                    <option value="Show Uncompleted">Uncompleted</option>
+                </select>
+            </div>
+
+        </div>
     );
 }
 
