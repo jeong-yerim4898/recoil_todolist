c0b80787478eedbe7c776887284c217db93ee4d9
yeriel-igaw <yeriel.jeong@igaworks.com>
2021-11-03 11:58:06 +0900

    document 를 react스럽게 변경 (Virtual Dom)

--- src/components/TodoChart/TodoChart.tsx
+++ src/components/TodoChart/TodoChart.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from 'react';
+import React, {JSXElementConstructor, SVGProps, useCallback, useEffect, useState} from 'react';
 import './TodoChart.scss';
 import {useRecoilValue} from "recoil";
 import {todoListStatsState} from "../../recoil/todo";
@@ -14,13 +14,13 @@ const MAX_DEGREE = 359.9;
 
 function TodoChart() {
 
-    const {totalNum, totalCompletedNum, totalUncompletedNum} = useRecoilValue(todoListStatsState);
+    const {totalCompletedNum, totalUncompletedNum} = useRecoilValue(todoListStatsState);
 
 
     const radius = 20; // 차트 반지름
     const diameter = 2 * Math.PI * radius;// 원 둘레
     const colors = ["#ff6554", "#ffdc2a", "#27ff3d", "#1f7cff", "#b611ff"];
-    const dataset = [totalCompletedNum,totalUncompletedNum];
+    const dataset = [totalCompletedNum, totalUncompletedNum];
 
     // 데이터의 총 합
     const total = dataset.reduce((r, v) => r + v, 0);
@@ -28,43 +28,28 @@ function TodoChart() {
     const acc = dataset.reduce((result, value) => [...result, result[result.length - 1] + value], [0]);
 
     // 원그리기
-   const drawCircle = useCallback(
+    /* const drawCirclePath = useCallback(
+         (): void => {
+             const svg = document.getElementById('pi-svg');
 
-        (): void => {
-            const svg = document.getElementById('pi-svg');
+             dataset.forEach((data: number, i: number) => {
+                 const radio = data / total; // 비율
+                 const fillSpace = diameter * radio; // 실제 차지하는 비율
+                 const emptySpace = diameter - fillSpace; // 전체에서 해당 데이터 차치 부분 빼고 나머지
+                 const offset = (acc[i] / total) * diameter; // 밀어주기
 
-            dataset.forEach((data: number, i: number) => {
-                const radio = data / total; // 비율
-                const fillSpace = diameter * radio; // 실제 차지하는 비율
-                const emptySpace = diameter - fillSpace; // 전체에서 해당 데이터 차치 부분 빼고 나머지
-                const offset = (acc[i] / total) * diameter; // 밀어주기
+                 // const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+                 // accDegree += MAX_DEGREE * radio;
+                 // path.setAttribute('d', `${getArc({x: 50, y: 50, radius: radius, degree: accDegree})}`);
+                 // path.setAttribute('fill', 'transparent');
+                 // path.setAttribute('stroke', colors[i]);
 
-                const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-                circle.setAttribute('cx', '50');
-                circle.setAttribute('cy', '50');
-                circle.setAttribute('r', String(radius));
-                circle.setAttribute('fill', 'transparent');
-                circle.setAttribute('stroke', colors[i]);
-                circle.setAttribute('stroke-width', '10');
-                circle.setAttribute('stroke-dasharray', `${fillSpace} ${emptySpace}`);
-                circle.setAttribute('stroke-dashoffset', String(-offset));
+                 // svg?.appendChild(path);
+             });
+         }
+         , [dataset]);
+ */
 
-                // const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-                // accDegree += MAX_DEGREE * radio;
-                // path.setAttribute('d', `${getArc({x: 50, y: 50, radius: radius, degree: accDegree})}`);
-                // path.setAttribute('fill', 'transparent');
-                // path.setAttribute('stroke', colors[i]);
-
-
-                svg?.appendChild(circle);
-                // svg?.appendChild(path);
-            });
-        }
-        , [dataset]);
-
-    useEffect(() => {
-        drawCircle();
-    }, [dataset]);
     // n도 벌어진 점의 좌표
     const getCoordsOnCircle = ({x, y, radius, degree}: ArcData) => {
         const radian = (degree / 180) * Math.PI;
@@ -88,10 +73,30 @@ function TodoChart() {
         return d;
     };
 
+    const DrawCircle = (): JSX.Element => {
+        return <>{
+            dataset.map((v, i) => {
+                const radio = v / total; // 비율
+                const fillSpace = diameter * radio; // 실제 차지하는 비율
+                const emptySpace = diameter - fillSpace; // 전체에서 해당 데이터 차치 부분 빼고 나머지
+                const offset = (acc[i] / total) * diameter; // 밀어주기
+                return <circle cx={50} cy={50} r={radius}
+                               stroke={colors[i]}
+                               strokeWidth={10}
+                               fill={"transparent"}
+                               strokeDasharray={`${fillSpace} ${emptySpace}`}
+                               strokeDashoffset={-offset}/>
+            })
+        }
+        </>
+    };
+
 
     return (
         <article className={'chart'}>
-            <svg width={500} height={500} viewBox={"0 0 100 100"} id={'pi-svg'}></svg>
+            <svg width={500} height={500} viewBox={"0 0 100 100"} id={'pi-svg'}>
+                <DrawCircle/>
+            </svg>
         </article>
     );
 }
