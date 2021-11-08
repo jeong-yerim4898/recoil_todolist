794aef0d0a6724b708414259a4b3fd9ff5e65922
yeriel-igaw <yeriel.jeong@igaworks.com>
2021-11-08 10:22:44 +0900

    Revert "도넛 차트 구현"
    This reverts commit be2ab1df288cd4efd6107c65b8ebe43365b83dd3.

--- src/components/TodoChart/TodoChart.tsx
+++ src/components/TodoChart/TodoChart.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, {JSXElementConstructor, SVGProps, useCallback, useEffect, useState} from 'react';
 import './TodoChart.scss';
 import {useRecoilValue} from "recoil";
 import {todoListStatsState} from "../../recoil/todo";
-
 
 interface ArcData {
     x: number;
@@ -18,38 +17,42 @@ function TodoChart() {
     const {totalCompletedNum, totalUncompletedNum} = useRecoilValue(todoListStatsState);
 
 
-    const radius = 80; // 차트 반지름
+    const radius = 20; // 차트 반지름
     const diameter = 2 * Math.PI * radius;// 원 둘레
-    const colors = ["#e02e1e", "#281B1AFF","#ff73cb","#fff75c","#93ff57"];
-    // const dataset = [totalCompletedNum, totalUncompletedNum,];
-    const dataset = [35,8,2,3,10];
+    const colors = ["#ff6554", "#ffdc2a", "#27ff3d", "#1f7cff", "#b611ff"];
+    const dataset = [totalCompletedNum, totalUncompletedNum];
+
     // 데이터의 총 합
     const total = dataset.reduce((r, v) => r + v, 0);
     // 도넛 시작점을 기억하기 위한  누적값
     const acc = dataset.reduce((result, value) => [...result, result[result.length - 1] + value], [0]);
 
-    const DrawCircle = (): JSX.Element => {
-        return <>{
-            dataset.map((v, i) => {
-                const radio = v / total; // 비율
-                const fillSpace = diameter * radio; // 실제 차지하는 비율
-                const emptySpace = diameter - fillSpace; // 전체에서 해당 데이터 차치 부분 빼고 나머지
-                const offset = (acc[i] / total) * diameter; // 밀어주기
-                return <path cx={50} cy={50} r={radius}
-                             stroke={colors[i]}
-                             strokeWidth={10}
-                             fill={"transparent"}
-                             strokeDasharray={`${fillSpace} ${emptySpace}`}
-                             strokeDashoffset={-offset}/>
-            })
-        }
-        </>
-    };
+    // 원그리기
+    /* const drawCirclePath = useCallback(
+         (): void => {
+             const svg = document.getElementById('pi-svg');
 
+             dataset.forEach((data: number, i: number) => {
+                 const radio = data / total; // 비율
+                 const fillSpace = diameter * radio; // 실제 차지하는 비율
+                 const emptySpace = diameter - fillSpace; // 전체에서 해당 데이터 차치 부분 빼고 나머지
+                 const offset = (acc[i] / total) * diameter; // 밀어주기
+
+                 // const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+                 // accDegree += MAX_DEGREE * radio;
+                 // path.setAttribute('d', `${getArc({x: 50, y: 50, radius: radius, degree: accDegree})}`);
+                 // path.setAttribute('fill', 'transparent');
+                 // path.setAttribute('stroke', colors[i]);
+
+                 // svg?.appendChild(path);
+             });
+         }
+         , [dataset]);
+ */
 
     // n도 벌어진 점의 좌표
     const getCoordsOnCircle = ({x, y, radius, degree}: ArcData) => {
-        const radian = ((degree-90)/ 180) * Math.PI;
+        const radian = (degree / 180) * Math.PI;
 
         return {
             x: x + radius * Math.cos(radian),
@@ -58,41 +61,31 @@ function TodoChart() {
     };
 
     const getArc = (data: ArcData) => {
-        const startCoord = getCoordsOnCircle({...data});
-        const finishCoord = getCoordsOnCircle({...data, degree: 0});
+        const startCoord = getCoordsOnCircle({...data, degree: 0});
+        const finishCoord = getCoordsOnCircle({...data});
 
 
         const {x, y, radius, degree} = data;
         const isLargeArc = degree > 180 ? 1 : 0;
+        const isEnd = degree === MAX_DEGREE;
 
-        return `M ${startCoord.x} ${startCoord.y} 
-        A ${radius} ${radius} 0 ${isLargeArc} 0 ${finishCoord.x} ${finishCoord.y} 
-        L ${x} ${y} `;
+        const d = `M ${startCoord.x} ${startCoord.y} A ${radius} ${radius} 0 ${isLargeArc} 1 ${finishCoord.x} ${finishCoord.y} L ${x} ${y} ${isEnd ? 'z' : ''}`;
+        return d;
     };
 
-    function getStartRotate(idx: number) {
-        return dataset.filter((_, i) => i < idx).reduce((all, v) => v + all, 0);
-    }
-
-
-    const DrawPi = (): JSX.Element => {
+    const DrawCircle = (): JSX.Element => {
         return <>{
             dataset.map((v, i) => {
-                const startRotate = getStartRotate(i);
                 const radio = v / total; // 비율
-                const rotateAngle = startRotate / total * MAX_DEGREE;
-                const d = getArc({x: 200, y: 200, radius, degree: radio * MAX_DEGREE});
-                const targetRad = 2 * Math.PI *radius* radio;
-                const targetRestRad = 2*Math.PI *radius *(1-radio);
-                return (
-                    <path d={`${d}`}
-                             key={i}
-                             stroke={colors[i]}
-                             strokeWidth={`40`}
-                             transform={`rotate(${rotateAngle},200,200) `}
-                             fill={'none'} strokeDasharray={`${targetRad} ${targetRestRad}`} strokeDashoffset={'3'}/>
-                )
-
+                const fillSpace = diameter * radio; // 실제 차지하는 비율
+                const emptySpace = diameter - fillSpace; // 전체에서 해당 데이터 차치 부분 빼고 나머지
+                const offset = (acc[i] / total) * diameter; // 밀어주기
+                return <circle cx={50} cy={50} r={radius}
+                               stroke={colors[i]}
+                               strokeWidth={10}
+                               fill={"transparent"}
+                               strokeDasharray={`${fillSpace} ${emptySpace}`}
+                               strokeDashoffset={-offset}/>
             })
         }
         </>
@@ -101,9 +94,8 @@ function TodoChart() {
 
     return (
         <article className={'chart'}>
-            <svg width={400} height={400} viewBox={"0 0 400 400"} id={'pi-svg'}>
-                {/*<DrawCircle/>*/}
-                <DrawPi/>
+            <svg width={500} height={500} viewBox={"0 0 100 100"} id={'pi-svg'}>
+                <DrawCircle/>
             </svg>
         </article>
     );
